ba99611f10c326c181a171a4c2109bbde7bef6cc
Daniel_Wong_SAIT <daniel.y.wong@edu.sait.ca>
2022-11-22 20:47:27 -0700

    navbar

--- client/src/components/CardNews.js
+++ client/src/components/CardNews.js
@@ -14,14 +14,14 @@ function CardNews () {
                 <div className={click ? 'news-circle-active' : 'news-circle'} onClick={handleClick}>
                     <i className={click ? 'fa-solid fa-angle-right' : 'fa-solid fa-angle-down'} onClick={handleClick}/>
                     <div className={click ? 'update-links-active' : 'update-links'}>
-                    <h3 id='update-h3'>Updates</h3>
-                    <CardItem text='News 1'
+                    <h3 id={click ? 'update-h3-active' : 'update-h3'}>Updates</h3>
+                    <CardItem className={click ? 'cardnews__item-active' : 'cardnews__item'} text='News 1'
                     name='news 1'/>
-                    <CardItem text='News 2'
+                    <CardItem className={click ? 'cardnews__item-active' : 'cardnews__item'} text='News 2'
                     name='news 2'/>
-                    <CardItem text='News 3'
+                    <CardItem className={click ? 'cardnews__item-active' : 'cardnews__item'} text='News 3'
                     name='news 3'/>
-                    <CardItem text='News 4'
+                    <CardItem className={click ? 'cardnews__item-active' : 'cardnews__item'} text='News 4'
                     name='news 4'/>
                     </div>
                 </div>
