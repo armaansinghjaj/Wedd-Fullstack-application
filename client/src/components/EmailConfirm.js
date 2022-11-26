ecb741f3209445afcfdc4972870611f1f7275a0d
shubh-preet-kaur <shubhsidhu777@gmail.com>
2022-11-26 08:56:44 -0700

    ..

---
+++ client/src/components/EmailConfirm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function EmailConfirm() {
+  return (
+    <div className="emailConfirm">
+     <p>
+        An email has been sent to you for password change.
+     </p> 
+        
+    </div>
+  )
+}
+
