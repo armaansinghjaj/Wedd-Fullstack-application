9c3a7a3726de8a96c6ca2d102ea5acb185f07245
bunny2030 <vaibhav.kumar@edu.sait.ca>
2022-11-27 07:13:47 -0700

    fix map on ride confirming page

--- client/src/components/Customer-components/Ridepage-components/Ride-process/ride-confirm.js
+++ client/src/components/Customer-components/Ridepage-components/Ride-process/ride-confirm.js
@@ -1,39 +1,46 @@
-import React from 'react';
-import Maps from '../Map/maps';
+import React, {useEffect, useState} from "react";
+import Maps from "../Map/maps";
 
 export default function RideConfirm() {
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-    
-    return(
-        <>
-        routing success
-        
-        <div id="map_div" >
-            {/* <%- include("map") -%> <br> */}
-            
-            <Maps></Maps>
-            <div id="estimate">
-                <button id="getquote">Get Quote</button>
-            </div>
-        </div>
-        <div id="info_div">
-            Please check these details<br/>
-            {/* <%= details %> */}
-        </div>
+	const [data, setData] = useState([]);
 
-        <input type="hidden" value="<%= startlat %>" name="startlat" id="startlat" />
-        <input type="hidden" value="<%= startlng %>" name="startlng" id="startlng" />
-        <input type="hidden" value="<%= destlat %>" name="destlat" id="destlat" />
-        <input type="hidden" value="<%= destlng %>" name="destlng" id="destlng" />
-        <input type="hidden" value="2" id="map_state" />
+	const fetchData = async () => {
+		const result_data = await fetch("/api/ride/processing");
+		const data_items = await result_data.json();
+		setData(data_items);
+	};
 
-        <div id="buttons">
-            <button id="get_map">Confirm</button><br/>
-            <form action="ride">
-                <input type="submit" value="Reset"/>
-            </form>
-        </div>	
-        
-        </>
-    )
+	return (
+		<>
+			<input type="hidden" value={data.name} name="hidden_name" id="hidden_name" />
+			<input type="hidden" value={data.email} name="hidden_email" id="hidden_email" />
+			<input type="hidden" value={data.phone} name="hidden_phone" id="hidden_phone" />
+			<input type="hidden" value={data.pickup} name="hidden_pickup" id="hidden_pickup" />
+			<input type="hidden" value={data.destination} name="hidden_destination" id="hidden_destination" />
+			<input type="hidden" value="2" id="map_state" />
+			routing success
+			<div id="map_div">
+				<Maps></Maps>
+				<div id="estimate">
+					<button id="getquote">Get Quote</button>
+				</div>
+			</div>
+			<div id="info_div">
+				Please check these details
+				<br />
+				<div id="details"></div>
+			</div>
+			<div id="buttons">
+				<button id="get_map">Confirm</button>
+				<br />
+				<form action="ride">
+					<input type="submit" value="Reset" />
+				</form>
+			</div>
+		</>
+	);
 }
