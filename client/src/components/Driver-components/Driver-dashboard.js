f4ee72a871c70a5fca62bb0890de6f0f3aa57f7d
Daniel_Wong_SAIT <daniel.y.wong@edu.sait.ca>
2022-12-04 22:40:44 -0700

    Driver maps css

--- client/src/components/Driver-components/Driver-dashboard.js
+++ client/src/components/Driver-components/Driver-dashboard.js
@@ -6,7 +6,6 @@ export default function Requests() {
 	const pickupAddress = "Pickup Address";
 	const dropoffAddress = "Dropoff Address";
 
-	const [visibleMap, setVisibleMap] = useState(false);
 	const [Loader, setLoader] = useState(false);
 	const [data, setData] = useState({name: "", email: "", phone: "", pickup: "", destination: ""});
 	useEffect(() => {
@@ -24,19 +23,16 @@ export default function Requests() {
 			});
 	}, []);
 
-	const displayMap = () => {
-		setVisibleMap(!visibleMap);
-	};
-
 	return (
 		<>
 			<input type="hidden" value={data.pickup_location} name="hidden_pickup" id="hidden_pickup" />
 			<input type="hidden" value={data.drop_location} name="hidden_destination" id="hidden_destination" />
 			<input type="hidden" value="3" id="map_state" />
-			<div id="Driverdash-container">
-				<div id="dashboard-wrapper">
-					<h2 id="dashboard-h2">Your ride</h2>
 
+			<div className={"map"}>
+					<Map />
+					<div id="dashboard-wrapper">
+					<h2 id="dashboard-h2">Your ride</h2>
 					<table id="dashboard-table">
 						<tbody>
 							<tr>
@@ -50,31 +46,18 @@ export default function Requests() {
 						</tbody>
 					</table>
 					<div className="starter">
-						{/* <p>Slide to drive</p>
-                        <p><input type="range" value="0" className="pullee"/></p> */}
-
 						<div id="buttons">
-							<p>
-								<button id="dashboard-show-map" onClick={displayMap}>
-									Show map
-								</button>
-							</p>
-							<form action="/DriverAvailRequests">
+							<form action="/driver/availrequests">
 								<input type="submit" id="dashboard-complete-ride" value="Complete Ride" />
 							</form>
-							<form action="/DriverAvailRequests">
+							<form action="/driver/availrequests">
 								<input type="submit" id="dashboard-cancel-ride" value="Cancel Ride" />
 							</form>
 						</div>
 					</div>
-					<div className={"map"}>
-						{/* <span id="map_section">MAP HERE</span> */}
-						<div id={visibleMap ? "map_placeholder" : "map_hidden"}>
-							<Map />
-						</div>
-					</div>
 				</div>
 			</div>
+
 		</>
 	);
 }
